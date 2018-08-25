c6efae1553a6f1b02cd2d9793f64c7a2ea058d2f
silverwind <me@silverwind.io>
2018-08-26 01:10:18 +0200

    parallelize tests

--- test.js
+++ test.js
@@ -4,13 +4,30 @@ const caa = require(".");
 const assert = require("assert");
 
 (async () => {
-  assert.equal((await caa("silverwind.io")).map(r => r.value).includes("letsencrypt.org"), true);
-  assert.equal((await caa("sub.silverwind.io")).map(r => r.value).includes("letsencrypt.org"), true);
-  assert.equal(await caa.matches("silverwind.io", "letsencrypt.org"), true);
-  assert.equal(await caa.matches("sub.silverwind.io", "letsencrypt.org"), true);
-  assert.equal(await caa.matches("caa-none.silverwind.io", "letsencrypt.org"), false);
-  assert.equal(await caa.matches("sub.caa-none.silverwind.io", "letsencrypt.org"), false);
-  assert.equal(await caa.matches("caa-wild.silverwind.io", "letsencrypt.org"), true);
-  assert.equal(await caa.matches("*.caa-wild.silverwind.io", "letsencrypt.org"), false);
-  assert.equal(await caa.matches("sub.caa-wild.silverwind.io", "letsencrypt.org"), true);
+  try {
+    const tests = [
+      {promise: caa("silverwind.io"), expect: records => records.map(r => r.value).includes("letsencrypt.org")},
+      {promise: caa("sub.silverwind.io"), expect: records => records.map(r => r.value).includes("letsencrypt.org")},
+      {promise: caa.matches("silverwind.io", "letsencrypt.org"), expect: true},
+      {promise: caa.matches("sub.silverwind.io", "letsencrypt.org"), expect: true},
+      {promise: caa.matches("caa-none.silverwind.io", "letsencrypt.org"), expect: false},
+      {promise: caa.matches("sub.caa-none.silverwind.io", "letsencrypt.org"), expect: false},
+      {promise: caa.matches("caa-wild.silverwind.io", "letsencrypt.org"), expect: true},
+      {promise: caa.matches("*.caa-wild.silverwind.io", "letsencrypt.org"), expect: false},
+      {promise: caa.matches("sub.caa-wild.silverwind.io", "letsencrypt.org"), expect: true},
+    ];
+
+    const results = await Promise.all(tests.map(test => test.promise));
+    for (const [i, result] of results.entries()) {
+      const expect = tests[i].expect;
+      if (typeof expect === "function") {
+        assert(tests[i].expect(result));
+      } else {
+        assert.equal(result, expect, `Test ${i} failed`);
+      }
+    }
+  } catch (err) {
+    console.info(err);
+    process.exit(1);
+  }
 })();
